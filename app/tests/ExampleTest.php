dfa85306930f1def1213fe1f48a2e8510180b286
Vaughn <alvaradovaughn@yahoo.com>
2016-02-27 16:06:36 -0600

    adding the fibonnaci equations and test for reference on how to use laravel with phpunit

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -3,20 +3,55 @@ class ExampleTest extends TestCase
 {
 
 
+
+	//----------------> These are tests using dataproviders to plug in numbers using fibonnaci function
 	/**
 	 * @dataProvider tVals()
 	 */
-	function testTrue( $testVal ) 
+	function testIfNumberIsAFibonnaci( $testVal ) 
 	{
-		$this->assertTrue( self::functionTest ( $testVal ) );
+		$this->assertTrue( self::isFibonacciBooleanTest ( $testVal ) );
 	}
 	/**
 	 * @dataProvider fVals()
 	 */
-	function testFalse( $testVal )
+	function testIfNumberIsNotAFibonnaci( $testVal )
 	{
-		$this->assertFalse( self::functionTest ($testVal ) );
+		$this->assertFalse( self::isFibonacciBooleanTest ( $testVal ) );
 	}
+
+	//---------------->>Data providers for the fibbonacci tests
+	function tVals()
+	{
+		return [
+			[ 89 ],
+			[ 8 ]   
+		];
+	}
+	function fVals()
+	{
+		return [
+			[ 5467 ]
+		];
+	}
+	//----------------->> Code to pass the fibonnaci tests
+	function isFibonacciBooleanTest($n)
+	{
+		 $firstFibonnaciEquation = sqrt(5 * $n * $n - 4);
+        if ($firstFibonnaciEquation - ceil($firstFibonnaciEquation) == 0) {
+            return true;
+        }
+
+        $secondFibonnaciEquation = sqrt(5 * $n * $n + 4);
+        if ($secondFibonnaciEquation - ceil($secondFibonnaciEquation) == 0) {
+            return true;
+        }
+        return false;
+	}
+
+//======================================================================> STARTING NEW TEST
+
+	//---------------> This is a test using a dataprovider to plug in numbers using the functionTest 
 	/**
 	 * @dataProvider eqVals()
 	 */
@@ -25,24 +60,14 @@ class ExampleTest extends TestCase
 		$this->assertEquals( $expected, self::functionTest ( $n, $c, $m ) );
 	}
 
-	function tVals()
-	{
-		return [
-			[  true]
-		];
-	}
-	function fVals()
-	{
-		return [
-			[ false]
-		];
-	}
+	//------------------>>Data provider for the functionTest
 	function eqVals()
 	{
 		return [
-			[ true]
+			[ ]
 		];
 	}	
+
 
 	function functionTest ( $val )
 	{
