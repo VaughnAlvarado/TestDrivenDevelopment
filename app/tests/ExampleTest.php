69277aa0058e4ee7a1c53477eccec97ffd18c12e
Vaughn <alvaradovaughn@yahoo.com>
2016-02-27 16:09:36 -0600

    refactoring equations

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -38,12 +38,10 @@ class ExampleTest extends TestCase
 	function isFibonacciBooleanTest($n)
 	{
 		$firstFibonnaciEquation = sqrt(5 * $n * $n - 4);
+		$secondFibonnaciEquation = sqrt(5 * $n * $n + 4);
         if ($firstFibonnaciEquation - ceil($firstFibonnaciEquation) == 0) {
             return true;
-        }
-
-        $secondFibonnaciEquation = sqrt(5 * $n * $n + 4);
-        if ($secondFibonnaciEquation - ceil($secondFibonnaciEquation) == 0) {
+        } else if ($secondFibonnaciEquation - ceil($secondFibonnaciEquation) == 0) {
             return true;
         }
         return false;
