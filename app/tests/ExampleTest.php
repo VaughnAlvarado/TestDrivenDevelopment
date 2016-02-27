3b1bf1b8ef90038476c5f60c544cdb9ce5fd32a0
Vaughn <alvaradovaughn@yahoo.com>
2016-02-27 16:08:01 -0600

    fixing tab margin

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -37,7 +37,7 @@ class ExampleTest extends TestCase
 	//----------------->> Code to pass the fibonnaci tests
 	function isFibonacciBooleanTest($n)
 	{
-		 $firstFibonnaciEquation = sqrt(5 * $n * $n - 4);
+		$firstFibonnaciEquation = sqrt(5 * $n * $n - 4);
         if ($firstFibonnaciEquation - ceil($firstFibonnaciEquation) == 0) {
             return true;
         }
